4587a3efc1b51943962b00733aa92171e087528a
Raduan-Nafiz <2221552@iub.edu.bd>
2024-04-16 01:28:41 +0600

    alertsUpdate

--- src/surveillanceofficerpkgRaduan/AlertsController.java
+++ src/surveillanceofficerpkgRaduan/AlertsController.java
@@ -6,7 +6,12 @@ package surveillanceofficerpkgRaduan;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
 /**
  * FXML Controller class
@@ -15,6 +20,17 @@ import javafx.fxml.Initializable;
  */
 public class AlertsController implements Initializable {
 
+    @FXML
+    private TextArea notificationList;
+    @FXML
+    private Button homePage;
+    @FXML
+    private Button recentBuyers;
+    @FXML
+    private Button recentSellers;
+    @FXML
+    private Button recentWithdraws;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +38,25 @@ public class AlertsController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    public AlertsController() {
+    }
+    private static final Logger LOG = Logger.getLogger(AlertsController.class.getName());
+
+    @FXML
+    private void goToHomePageOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void recentBuyersOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void recentSellersOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void recentWithdrawsOnClick(ActionEvent event) {
+    }
     
 }
